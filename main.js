cffb6065cdead6eeabe7cf2cb3a39e76c15645e8
Luis <s.ros.lui@gmail.com>
2020-12-02 19:52:48 +0100

    add vue

--- main.js
+++ main.js
@@ -10,6 +10,28 @@ const web = new Vue({
             {nome: "Blog"},
             {nome: "Shop"},
             {nome: "Elements"}
+        ],
+
+        contattoPersonale: {
+            email: "maree.qode@gmail.com",
+            tel: "+44645321789"
+        },
+
+        indirizzo: {
+            via: "Avenue d'Auderghem 10",
+            citta: "1040 Brussels, Belgium"
+        },
+
+        linkSocial:{
+            frase:"Stay in touch with us"
+        },
+
+        ArraySocial: [
+            {nome: "twitter", url: "fab fa-twitter"},
+            {nome: "pinterest", url:"fab fa-facebook-f"},
+            {nome:"facebook", url:"fab fa-facebook-f"},
+            {nome: "linkedin", url:"fab fa-linkedin-in"}
         ]
+
     }
 });
