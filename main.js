53f8c99cb552e6ad9ef01cc7894cab6dd544d014
Luis <s.ros.lui@gmail.com>
2020-12-02 18:02:30 +0100

    add vue sezioni nav

---
+++ main.js
@@ -0,0 +1,15 @@
+// console.log("prova prova, sa sa");
+
+const web = new Vue({
+    el: "#web",
+    data: {
+        navBarSezioni: [
+            {nome: "Home"},
+            {nome: "Pages"},
+            {nome: "Portfolio"},
+            {nome: "Blog"},
+            {nome: "Shop"},
+            {nome: "Elements"}
+        ]
+    }
+});
